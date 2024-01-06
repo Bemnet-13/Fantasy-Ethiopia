12a2a569ec3c4450097e9fb61ed0f420f9152203
techkelster <techkelster@gmail.com>
2024-01-06 09:33:19 +0300

    tailwindsetup and some folder rearragment menu.html started

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("mobile-menu-button")
+  .addEventListener("click", function () {
+    var mobileMenu = document.getElementById("mobile-menu");
+    mobileMenu.classList.toggle("hidden");
+  });
