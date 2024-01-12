eb386c61eabcce9871139db0ac81479bcc4e58ec
sofc-t <sofstzz@gmail.com>
2024-01-12 10:15:06 +0300

    auth final

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,40 @@
+import { Body, Controller, Delete, Get, Post, Put, Param, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignUpDto } from './dto/signup.dto';
+import { User } from './schemas/user.schema';
+import { ModifyUserDto} from './dto/modify-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Get('/login')
+  
+  login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
+  }
+
+  @Put(':id')
+    async updateUser(
+      @Param('id')
+      id: string,
+      @Body()
+      upUser: ModifyUserDto,
+    ): Promise<User> {
+      const user = upUser as User;
+      return this.authService.updateById(id, user);
+    }
+    @Delete(':id')
+    async deleteUser(
+      @Param('id')
+      id: string,
+    ): Promise<User> {
+      return this.authService.deleteById(id);
+    }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,90 @@
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+
+import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { Player } from 'src/players/schemas/player.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
+    const { name, email, password } = signUpDto;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
+
+  async login(loginDto: LoginDto): Promise<{ token: string }> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    return { token };
+  }
+
+  async updateById(id: string, user: User): Promise<User> {
+    return await this.userModel.findByIdAndUpdate(id, user, {
+      new: true,
+      runValidators: true,
+    });
+  }
+  async deleteById(id: string): Promise<User> {
+    return await this.userModel.findByIdAndDelete(id);
+  }
+  async findById(id: string): Promise<User> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException('Please enter correct id.');
+    }
+
+    const user = await this.userModel.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('Player not found.');
+    }
+
+    return user;
+  }
+
+  async validateToken(token: string): Promise<any> {
+    try {
+      const decoded = this.jwtService.verify(token);
+      return decoded;
+    } catch (error) {
+      console.error('Invalid token:', error.message);
+      return null;
+    }
+  }
+}
